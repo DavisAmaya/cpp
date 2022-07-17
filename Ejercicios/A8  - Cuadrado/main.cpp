ba541c62d459be62c0f60414b7de1a6379ce2114
Davis Amaya <105960269+DavisAmaya@users.noreply.github.com>
2022-07-16 18:55:12 -0600

    Update main.cpp

--- Ejercicios/A8  - Cuadrado/main.cpp
+++ Ejercicios/A8  - Cuadrado/main.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 int main(int argc, char const *argv[])
+    
 {
     double numero1 = 5;
     double numero2 = 0;
     
-    //Funcion sqrt que calcula e imprime la raiz cudrada de un numero dado
 
     numero2 = sqrt (numero1);
 
