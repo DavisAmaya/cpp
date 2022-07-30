151df33a331d10dcfcaa2fd8ad0e7dc85306a5c1
Davis Amaya <105960269+DavisAmaya@users.noreply.github.com>
2022-07-29 18:05:22 -0600

    Update notas completas.cpp

--- Ejercicios/Ejercicio15- Notas - completas/notas completas.cpp
+++ Ejercicios/Ejercicio15- Notas - completas/notas completas.cpp
@@ -9,60 +9,48 @@ int main(int argc, char const *argv[])
     cout << "Ingrese la nota que obtuvo: ";
     cin >> nota;
 
-    if (nota > 100 || nota < 0)
-    {
+    if (nota > 100 || nota < 0){
         cout << "Ingrese una nota entre 0 - 100 "; 
         return 0;
     }
 
-    if (nota >= 96 && nota <= 100)
-    {
-        cout << "Obtuviste una A++ "; 
-        
+    if (nota >= 96 && nota <= 100){
+        cout << "Obtuviste una A++ ";  
     }
     
-    if (nota >= 91 && nota <= 95)
-    {
+    if (nota >= 91 && nota <= 95){
         cout << "Obtuviste una A+ "; 
     }
 
-    if (nota >= 91 && nota <= 95)
-    {
+    if (nota >= 91 && nota <= 95){
         cout << "Obtuviste una A+ "; 
     }
 
-    if (nota >= 86 && nota <= 90)
-    {
+    if (nota >= 86 && nota <= 90){
         cout << "Obtuviste una A "; 
     }
 
-    if (nota >= 81 && nota <= 85)
-    {
+    if (nota >= 81 && nota <= 85){
         cout << "Obtuviste una A- "; 
     }
 
-    if (nota >= 76 && nota <= 80)
-    {
+    if (nota >= 76 && nota <= 80){
         cout << "Obtuviste una B+ "; 
     }
 
-    if (nota >= 71 && nota <= 75)
-    {
+    if (nota >= 71 && nota <= 75){
         cout << "Obtuviste una B "; 
     }
 
-    if (nota >= 66 && nota <= 70)
-    {
+    if (nota >= 66 && nota <= 70){
         cout << "Obtuviste una B- "; 
     }
 
-    if (nota >= 61 && nota <= 65)
-    {
+    if (nota >= 61 && nota <= 65){
         cout << "Obtuviste una C+ "; 
     }
 
-    if (nota >= 56 && nota <= 60)
-    {
+    if (nota >= 56 && nota <= 60){
         cout << "Obtuviste una C "; 
     }
 
