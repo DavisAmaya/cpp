d4c5edd62a589176e17b5c6dfe72783be9d0cbd3
Davis Amaya <105960269+DavisAmaya@users.noreply.github.com>
2022-07-29 21:12:50 -0600

    Update main.cpp

--- Ejercicios/Ejercicio48  - Funciones-recursivas/main.cpp
+++ Ejercicios/Ejercicio48  - Funciones-recursivas/main.cpp
@@ -14,18 +14,16 @@ void adivinarNumeroSecreto (int miNumero)
     {
         cout << "Intento fallado con " << miNumero << endl;
         int otroNumero = 0;
+        
         cout << "Ingrese otro numero: " << endl;
         cin >> otroNumero;
 
         adivinarNumeroSecreto (otroNumero);
-    }
-    
+    } 
 }
 
 int main(int argc, char const *argv[])
 {
     adivinarNumeroSecreto (5);
-    
-
     return 0;
 }
