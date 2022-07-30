294b766f893935a95f3da03de5562bf0cc99274d
Davis Amaya <105960269+DavisAmaya@users.noreply.github.com>
2022-07-29 18:24:20 -0600

    Update main.cpp

--- Ejercicios/Ejercicio31 - Calculadora - con - funciones/main.cpp
+++ Ejercicios/Ejercicio31 - Calculadora - con - funciones/main.cpp
@@ -4,14 +4,12 @@ using namespace std;
 
 int suma ( int a, int b )
 {
-
     return a + b;
 }
 
 int calculadora ( int a, int b, char operador)
 {
-    if (operador == '+')
-    {
+    if (operador == '+'){
         return suma (a, b);
     }
     return -1;
@@ -22,18 +20,13 @@ int main(int argc, char const *argv[])
     int n1 = 0;
     int n2 = 0;
     char operador;
-
     cout << "Ingrese a: ";
     cin >> n1;
-
     cout << "Ingrese b: ";
     cin >> n2;
-    
     cout << "Ingrese el operador ( +, -, *, / ): ";
     cin >> operador;
-
     cout << endl;
-
     cout << "Resultado de " << n1 << operador << n2 << " es: ";
     cout << calculadora (n1, n2, operador);
 
