545d0974d4a7b92b166c5c81600df49f8644e059
Davis Amaya <105960269+DavisAmaya@users.noreply.github.com>
2022-07-29 18:27:14 -0600

    Update main.cpp

--- Ejercicios/Ejercicio34 - NombreCompleto/main.cpp
+++ Ejercicios/Ejercicio34 - NombreCompleto/main.cpp
@@ -13,12 +13,9 @@ int main(int argc, char const *argv[])
 
     cout << "Ingrese su primer nombre:   ";
     cin >> primerNombre;
-
     cout << "Ingrese su primer apellido:   ";
     cin >> primerApellido;
-
     cout << endl;
-
     cout << "Nombre completo: " << nombreCompleto (primerNombre, primerApellido);
 
     return 0;
